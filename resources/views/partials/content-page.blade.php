a33f7ab102f70c9fc338f0b6d9fb9487d66f5dd0
El dano <danielpliego@gmail.com>
2018-10-04 23:25:38 -0500

    first commit

---
+++ resources/views/partials/content-page.blade.php
@@ -0,0 +1,123 @@
+<div class="hero bg-dark text-light py-5 text-center">
+
+  <div class="container">
+    <div class="py-5">
+      <h1>
+        TULUM
+        <small class="d-block">JANUARY 8, 2019</small>
+      </h1>
+      <h4 class="text-uppercase">CASA IKAL, Sian Ka‘an BIOSPHERE RESERVE KM 12</h4>
+    </div>
+    <div class="py-5">
+      <p class="lead text-uppercase">
+        In the recent decade, the mesoamerican reef has experienced a density loss of 75% endangering  hundreds of marine species.
+      </p>
+      <p class="lead text-uppercase">
+        For this edition, Y’am is partnering with restore coral an organization that creates, maintains and multiplies coral farms for the restoration of damaged reefs.
+      </p>
+    </div>
+    <div class="embed-responsive embed-responsive-16by9 my-5">
+      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/u7-20aHePeU?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="embed-responsive-item"></iframe>
+    </div>
+  </div>
+
+</div>
+
+<div class="venue bg-primary text-light py-5 text-center">
+
+  <div class="container">
+    <div class="py-5">
+      <h2 class="mb-5 text-uppercase">The Venue</h2>
+      <p class="lead text-uppercase">
+        A stunning beach front villa inside the biosphere reserve of Sian Ka’an, a mile north of Boca Paila bridge and 20 mins away from Tulum; the Caribbean beach-front brings you to a rich coral reef at swimming distance and the lagoon front goes directly to ancient Mayan canals, one of the most spectacular and exclusive places on earth!.
+      </p>
+      <p class="lead text-uppercase">      
+        The Villa also sets new standards of sustainability and it is completely auto sufficient - sun, wind power and a very silent backup generator - zero carbon property, but with all the modern comforts.
+      </p>
+      <p class="lead text-uppdercase">
+        An ultimate villa in the Mexican Caribbean!
+      </p>
+    </div>
+    <div class="row no-gutters align-items-center mb-5">
+      <div class="col">
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/venue/1.png" class="img-fluid" />
+      </div>
+      <div class="col">
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/venue/2.png" class="img-fluid" />
+      </div>
+      <div class="col">
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/venue/1.png" class="img-fluid" />
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="experience bg-danger text-light py-5 text-center">
+
+  <div class="container">
+    <div class="py-5">
+      <h2 class="mb-5 text-uppercase">The Experience</h2>
+    </div>
+
+    <div class="row no-gutters align-items-center my-5">
+      <div class="col-md-7">
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/experience/coral-farming.png" class="img-fluid" />
+      </div>
+      <div class="col-md-5 text-left">
+        <h4>CORAL FARMING</h4>
+        <p class="lead-text-uppercase">
+          DIVING EXPERIENCE IN COZUMEL, MEXICO.
+        <br><br>
+          - The experience includes transport: tulum - cozumel, cozumel - tulum
+        <br><br>
+          - Corals are collected from local reefs and then installed at the restoration site.
+        </p>
+      </div>
+    </div>
+
+    <div class="row no-gutters align-items-center my-5">
+      <div class="col-md-7 order-lg-2">
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/experience/vr-diving.png" class="img-fluid" />
+      </div>
+      <div class="col-md-5 text-left">
+        <h4>VR DIVING</h4>
+        <p class="lead-text-uppercase">
+          Virtual reality immersive experience 
+          <br><br>
+          - sensitize the public about the need to protect coral reefs. 
+          <br><br>
+          - learn and discus about how to increase sustainable ocean related activities  
+        </p>
+      </div>
+    </div>
+
+    <div class="row no-gutters align-items-center my-5">
+      <div class="col-md-7">
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/experience/vr-diving.png" class="img-fluid" />
+      </div>
+      <div class="col-md-5 text-left">
+        <h4>SHABAT DINNER</h4>
+        <p class="lead-text-uppercase">
+          - recognized chef, cesar castañeda will  COOK A THEMATIC DINNER while we host the ceremony and blessing. 
+        </p>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+<div class="experience bg-warning text-light py-5 text-center">
+
+  <div class="container">
+    <div class="py-5">
+      <h2>LINEUP</h2>
+      <p>Admisions open, full ticket info and hotels coming soon…</p>
+      <a href="#" class="btn btn-primary btn-lg">
+        APPLY FOR TICKETS
+      </a>
+    </div>
+
+    <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/criminal-logo.png" width="143" class="img-fluid" />
+  </div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand d-block d-md-none" href="#"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/yam-logo.png" class="img-fluid" height="35" /></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav justify-content-center w-100">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">TULUM.</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ACAPULCO.</a>
+      </li>
+      <li class="nav-item d-none d-md-block">
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/yam-logo.png" class="img-fluid" />
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">MYKONOS.</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT.</a>
+      </li>
+    </ul>
+  </div>
+</nav>
