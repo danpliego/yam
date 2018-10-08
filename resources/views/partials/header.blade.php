f42be019695513dba07640c19f181661633ff4a8
El dano <danielpliego@gmail.com>
2018-10-08 08:22:37 -0500

    changes from meeting

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,21 +5,41 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav justify-content-center w-100">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">TULUM.</a>
+    <ul class="navbar-nav justify-content-center w-100 align-items-center">
+      <li class="nav-item active dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          TULUM.
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">2019</a>
+          <a class="dropdown-item" href="#">2018</a>
+        </div>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">ACAPULCO.</a>
       </li>
-      <li class="nav-item d-none d-md-block">
-        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/yam-logo.png" class="img-fluid" />
+      <li class="nav-item mx-3 d-none d-md-block">
+        <a href="#">
+          <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/yam-logo.png" height="35px" />
+        </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">MYKONOS.</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">ABOUT.</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav navbar-social-media">
+      <li class="nav-item">
+        <a href="https://www.facebook.com/YAMCELEBRATION/" class="nav-link px-3">
+          <i class="fab fa-facebook"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="https://www.instagram.com/yamcelebration/" class="nav-link px-3">
+          <i class="fab fa-instagram"></i>
+        </a>
       </li>
     </ul>
   </div>
